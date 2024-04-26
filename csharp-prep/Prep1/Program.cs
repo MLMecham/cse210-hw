716a29d535ac57b8d20bababf5035e9a06a0918b
MLMecham <141757176+MLMecham@users.noreply.github.com>
2024-04-26 13:32:42 -0600

    Finished

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        
+        // Console.WriteLine("Hello Prep1 World!");
+        // Console.Write("You are my friend, ");
+        // Console.WriteLine("You are my sunshine");
+
+        // Console.WriteLine("How old are you");
+        // string age = Console.ReadLine();
+        // int int_age = int.Parse(age);
+
+        // if (int_age < 21)
+        // {
+        //     Console.WriteLine("You may not enter the secret club");
+        // }
+        // else if (int_age == 21)
+        // {
+        //     Console.WriteLine("You are barely old enough");
+        // }
+        // else 
+        // {
+        //     Console.WriteLine("You are very old. Enter please");
+        // }
+
+        // int age_plus_two = int_age + 2;
+        // Console.WriteLine($"In two years you will be {age_plus_two} years old");
+
+
+        //I know that you mentioned that we should put our innitials into our variables. I hope this is what you meant
+
+        string ML_first_name;
+        string ML_last_name;
+
+        Console.Write("What is your first name? ");
+        ML_first_name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        ML_last_name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {ML_last_name}, {ML_first_name} {ML_last_name}");
     }
 }
