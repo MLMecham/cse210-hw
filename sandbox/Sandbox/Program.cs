8a2607e33ed719154209692055bb20ab3729de4a
MLMecham <141757176+MLMecham@users.noreply.github.com>
2024-05-03 18:01:53 -0600

    Finished Prep3-5

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // Console.WriteLine("Hello Sandbox World!");
+
+        List<object> myList = new();
+        myList.Add("Super cool string");
+        myList.Add(55);
+
+        foreach (object item in myList)
+        {
+            Console.WriteLine(item);
+        }
+
+
     }
 }
