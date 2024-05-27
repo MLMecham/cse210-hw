191fef6cfd8a34ed61a91fa2795b500d2537d2e5
MLMecham <141757176+MLMecham@users.noreply.github.com>
2024-05-26 21:44:24 -0600

    Added loops to the program. All that is needed is a way to count how many words changed in every loops so that three words really change and a way to count how many times the program doesn't change a word and if not count to end the loop if the program doesn't change a word 1000 times in a row.

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -9,38 +9,69 @@ class Program
         // This will become the scripture to memorize.
         // I will make a way to select from a list.
         string scriptureToMemorize = "Hello to all. Goodbye everybody.";
+        scriptureToMemorize = "And it came to pass that I, Nephi, said unto my father: I will go and do the things which the Lord hath commanded, for I know that the Lord giveth no commandments unto the children of men, save he shall prepare a way for them that they may accomplish the thing which he commandeth them.";
         Scripture scripture1 = new Scripture(scriptureToMemorize);
         Reference referecne1 = new Reference("1 Nephi", 3, 7, 7);
         Word word1 = new Word();
         string currentWord;
 
-
-        // This will be the loop
-        scripture1.SetRandom();
-        
-
-        currentWord = scripture1.GetRandomWord();
-        word1.SetWord(currentWord);
-        
-
-        // Hide the word
-        word1.HideWord();
-        currentWord = word1.GetWord();
-        
-
-        // Return the word to the verse
-        scripture1.SetRandomWord(currentWord);
-        scripture1.ReturnHiddenWord();
-        scripture1.SetVerse();
-
-
-
-
+        // Show the initial full scripture and reference
+        Console.WriteLine("Try to memorize this scripture!");
         referecne1.Display();
         scripture1.Dispaly();
-        // scriputre1.DisplayList();
 
 
+        Console.WriteLine(Environment.NewLine);
+        Console.Write("Press Enter to remove three words or type 'quit' to exit the program.     ");
+        string enterKey = Console.ReadLine();
+        Console.WriteLine(Environment.NewLine);
+
+        // This will be the loop that checks for continue or quit
+        do
+        {
+            
+            int word_count = 0;
+
+            // This will be the loop that counts changes in the words
+            do
+                {
+                    scripture1.SetRandom();
+                
+
+                currentWord = scripture1.GetRandomWord();
+                word1.SetWord(currentWord);
+                
+
+                // Hide the word
+                word1.HideWord();
+                currentWord = word1.GetWord();
+                
+
+                // Return the word to the verse
+                scripture1.SetRandomWord(currentWord);
+                scripture1.ReturnHiddenWord();
+                scripture1.SetVerse();
+
+
+
+
+                
+                
+                word_count = word_count + 1;
+                // scriputre1.DisplayList();
+            } while (word_count < 3);
+
+            // Display the scripture again to memorize.
+            referecne1.Display();
+            scripture1.Dispaly();
+
+            Console.WriteLine(Environment.NewLine);
+            Console.Write("Press Enter to remove three words or type 'quit' to exit the program.     ");
+            enterKey = Console.ReadLine();
+            
+
+
+        } while (enterKey != "quit");
 
 
 
